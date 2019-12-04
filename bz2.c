4b10b56d14a3747f624aca69d5f47ab719cff926
Pedro Igor <pedroigor.silva@outlook.com>
2019-12-03 21:57:07 -0300

    Alterando formatção do nome do arquivo.
    Co-authored-by: Caio Fernandes <caiovf13@gmail.com>
    Co-authored-by: Lucas Gomes <lucas.gomesgs0@gmail.com>

--- bz2.c
+++ bz2.c
@@ -8,8 +8,38 @@ char DIRECTORIES[1000000][200];
 long long int DIR_POS = 0;
 char DIRECTORY_NAME[1000000];
 char ZIP_NAME[1000000];
+char TEMP_NAME[1000000];
 FILE *terminal;
 
+void copy_files(char *dirname){
+    DIR *dir;
+    struct dirent *dirp;
+    dir=opendir(dirname);
+    chdir(dirname);
+    
+    while((dirp=readdir(dir))!=NULL){
+        if(dirp->d_type==4){
+            if(strcmp(dirp->d_name, ".")==0 || strcmp(dirp->d_name, "..")==0){
+                continue;
+            }
+            //printf("%s %s\n", "FOLDER", dirp->d_name);
+            find_files(dirp->d_name);
+        }
+        else{
+            char zip[255] = "bzip2 ";
+            strcat(zip, dirp->d_name);
+            //printf("%s\n", zip);
+            char path[PATH_MAX];
+            terminal = popen(zip, "r");
+            while(fgets(path, PATH_MAX, terminal) != NULL);
+            //printf("%s %s\n", "FILE", dirp->d_name);
+        }
+        strcpy(DIRECTORIES[DIR_POS], dirp->d_name);
+        DIR_POS++;
+    }
+    chdir("..");
+    closedir(dir);
+}
 void find_files(char *dirname){
     DIR *dir;
     struct dirent *dirp;
@@ -67,20 +97,19 @@ void remove_dirs(char *dirname){
 }
 
 int main( int argc, char *argv[ ]){
-    strcpy(DIRECTORY_NAME, argv[1]);
-    strcpy(ZIP_NAME, argv[2]);
-    int size = strlen(ZIP_NAME);
-    ZIP_NAME[size-4] = '\0';
-    char destino_tar_name[255];
-    strcpy(destino_tar_name, ZIP_NAME); //destino, já removemos o .tar
-    strcat(destino_tar_name, ".bz2.tar"); //nome final do arquivo
-    strcat(ZIP_NAME, ".bz2");
+    strcpy(DIRECTORY_NAME, argv[1]); //Caminho atual
+    strcpy(ZIP_NAME, argv[2]); //Nome do arquivo final
+    int size = strlen(ZIP_NAME);  //Quantidade de caracteres arquivo 
+    strcpy(TEMP_NAME, ZIP_NAME); // TEMP_NAME == ZIP_NAME
+    TEMP_NAME[size-4]='\0'; //Quebra linha no antes do .tar
     find_files(DIRECTORY_NAME);
     char tar[255] = "tar cf ";
-    strcat(tar, destino_tar_name);
+    rename(DIRECTORY_NAME, TEMP_NAME); //Altera o nome do arquivo recebi para o arquivo de saida
+    strcpy(DIRECTORY_NAME, TEMP_NAME);  
+    //strcpy(DIRECTORY_NAME, ZIP_NAME);
+    strcat(tar, ZIP_NAME);
     strcat(tar, " ");
     strcat(tar, DIRECTORY_NAME);
-    printf("%s\n", tar);
     terminal = popen(tar, "r");
     char *path;
     while(fgets(path, PATH_MAX, terminal) != NULL);
